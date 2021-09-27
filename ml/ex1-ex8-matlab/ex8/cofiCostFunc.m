c52fb65d37b34261c20a592880aa0dddeb381328
vc <btk@zju.edu.cn>
2021-09-27 22:00:33 +0800

    finish exercise8

--- ml/ex1-ex8-matlab/ex8/cofiCostFunc.m
+++ ml/ex1-ex8-matlab/ex8/cofiCostFunc.m
@@ -40,8 +40,20 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+J = sum(sum(((X * Theta' - Y) .* R) .^ 2)) / 2 + lambda / 2 * (sum(sum(Theta .^ 2)) + sum(sum(X .^ 2)));
+for i = 1:num_movies
+    idx = find(R(i, :) == 1);
+    Theta_temp = Theta(idx, :);
+    Y_temp = Y(i, idx);
+    X_grad(i, :) = (X(i, :) * Theta_temp' - Y_temp) * Theta_temp + lambda * X(i, :);
+end
 
-
+for j = 1:num_users
+    idx = find(R(:, j) == 1);
+    X_temp = X(idx, :);
+    Y_temp = Y(idx, j);
+    Theta_grad(j, :) = (X_temp * Theta(j,:)' - Y_temp)' * X_temp + lambda * Theta(j, :); 
+end
 
 
 
